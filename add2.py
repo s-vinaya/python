dfb4bf4533fb66e2c786593da2c42be13cff2840
S Vinaya <131242389+s-vinaya@users.noreply.github.com>
2023-08-04 12:30:01 +0530

    Create add2.py

---
+++ add2.py
@@ -0,0 +1,4 @@
+a=int(input(“enter the first number:”)) //input first number
+b=int(input(“enter the second number:”)) //input second number
+sum=a+b //addition of two numbers
+print(“sum:”, sum) //the required sum is printed
