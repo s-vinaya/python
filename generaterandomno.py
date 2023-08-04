ab6bb7faddd4d2cc0a2b77f2ea67c710f1f349a6
S Vinaya <131242389+s-vinaya@users.noreply.github.com>
2023-08-04 12:19:57 +0530

    Create generaterandomno.py

---
+++ generaterandomno.py
@@ -0,0 +1,12 @@
+import random as rd
+
+print(rd.random())
+#gives a random float number
+print(rd.randint(1,100))
+#gives a random integer between these 2 numbers
+
+print(rd.randrange(1,100))
+#gives a random integer in this range
+
+print(rd.choice([1,2,3,4,5,6,7]))
+#gives a random integer in these choices
